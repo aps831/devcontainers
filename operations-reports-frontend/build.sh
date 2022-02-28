610d76e37e8653cf85c10e4e1bc83bc657b12e22
Andrew Spratley <aps831@yahoo.co.uk>
2022-02-28 12:24:57 +0000

    refactor: fix trivy issues

--- operations-reports-frontend/build.sh
+++ operations-reports-frontend/build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 IMAGE_NAME="ghcr.io/aps831/devcontainers/operations-reports-frontend:latest"
-npx -p @vscode/dev-container-cli devcontainer build --image-name $IMAGE_NAME .
+npx -p @vscode/dev-container-cli devcontainer build --image-name "$IMAGE_NAME" .
