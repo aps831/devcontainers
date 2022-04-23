c08f88434cf4824b5f84f246a521865fddf78421
Andrew Spratley <aps831@yahoo.co.uk>
2022-04-23 20:49:01 +0100

    feat: add additional tools to base

--- base/build.sh
+++ base/build.sh
@@ -4,10 +4,12 @@ DEBIAN_VERSION="debian-11"
 GITLEAKS_VERSION="8.5.1"
 GIT_MKVER_VERSION="1.2.2"
 NODE_VERSION="node_16.x"
+DELTA_VERSION="0.12.1"
 docker build --build-arg DEBIAN_VERSION=$DEBIAN_VERSION \
              --build-arg GITLEAKS_VERSION=$GITLEAKS_VERSION \
              --build-arg GIT_MKVER_VERSION=$GIT_MKVER_VERSION \
              --build-arg NODE_VERSION=$NODE_VERSION \
+             --build-arg DELTA_VERSION=$DELTA_VERSION \
              --progress=plain \
              --tag $IMAGE_NAME \
              .devcontainer
