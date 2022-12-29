e1c318209409d70042eb5a63029666973fc167b7
Andrew Spratley <aps831@yahoo.co.uk>
2022-12-29 17:30:55 +0000

    chore: ensure latest dev-container-cli used

--- go/build.sh
+++ go/build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 IMAGE_NAME="ghcr.io/aps831/devcontainers/go:latest"
-npx -p @vscode/dev-container-cli devcontainer build --image-name "$IMAGE_NAME" .
+npx -p @vscode/dev-container-cli@latest devcontainer build --image-name "$IMAGE_NAME" .
